1707017e6fd2ef4a9cdfb340ebd1c49d39ab31b2
Aizazullah <93179083+MetalExchangeServices@users.noreply.github.com>
2024-04-01 15:41:09 +0500

    home page fixed

--- components/container/HeroSection.tsx
+++ components/container/HeroSection.tsx
@@ -1,7 +1,5 @@
 "use client";
 import React from "react";
-import NextImage from "next/image";
-import { homeBanner } from "@/public/images";
 import { AppContainer } from "../layout/AppContainer";
 import { Button } from "@nextui-org/react";
 import { store } from "@/redux/store";
@@ -9,22 +7,15 @@ import { changeModelStatus } from "@/redux/model/controller";
 
 const HeroSection = () => {
   return (
-    <section className="w-full h-screen relative">
-      <NextImage
-        src={homeBanner}
-        alt="home-banner"
-        quality={1}
-        className="w-full h-full object-cover"
-      />
-      <div
-        className="w-full h-full top-0 left-0 z-10 absolute"
-        style={{
-          boxShadow: "inset #0000004d 500px 0px 100px 40px",
-        }}
-      >
+    <section className="w-full h-[calc(100vh-154px)] relative">
+      <div className="w-full h-full top-0 left-0 z-10 absolute">
         <AppContainer className="flex flex-col h-full justify-center">
           <h1 className="text-5xl leading-[125%] font-semibold capitalize">
-            Participates for <br /> weekly prizes <br /> in your favorite games
+            Play whatever,
+            <br />
+            wherever with
+            <br />
+            whoever
           </h1>
           <h3 className="text-base pt-2 opacity-85">
             This can be useful when you want to remove filters conditionally,
--- components/layout/HomeNavbar.tsx
+++ components/layout/HomeNavbar.tsx
@@ -1,42 +1,13 @@
-"use client";
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { AppContainer } from "./AppContainer";
-import { store } from "@/redux/store";
-import { showChallengeThunk } from "@/redux/toast/controller";
 import UserAvatar from "./UserAvatar";
 import SafetyLayer from "./SafetyLayer";
 
 const HomeNavbar = () => {
-  const [scrolled, setScrolled] = useState<boolean>(false);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    };
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <header
-      aria-checked={scrolled}
-      className="w-full fixed duration-200 top-0 bg-transparent z-40 aria-checked:bg-[hsla(0,0%,10%,0.9)] aria-checked:backdrop-blur border-b-[hsla(0,0%,16%)] aria-checked:border-b-1"
-    >
+    <header className="w-full sticky bg-[hsla(0,0%,10%)] duration-200 top-0 z-40">
       <AppContainer className="h-20 flex items-center justify-between">
         <h3
-          onClick={() => {
-            store.dispatch(
-              showChallengeThunk(
-                "Hi, you just receive a request from xyz@gmail.com"
-              )
-            );
-          }}
           className="text-2xl font-semibold text-nowrap"
           style={{ letterSpacing: "4px" }}
         >
