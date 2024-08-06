c13ffdc1e43920a40c28ae34ae4f87e01cb342db
Aizazullah <93179083+MetalExchangeServices@users.noreply.github.com>
2024-08-06 08:14:34 +0500

    text update

--- components/container/HeroSection.tsx
+++ components/container/HeroSection.tsx
@@ -4,16 +4,14 @@ import { AppContainer } from "../layout/AppContainer";
 import GameCard from "../card/GameCard";
 import CommingSoonCard from "../card/CommingSoon";
 import LeaderBoardModel from "../models/LeaderBoard";
-import useSWR, { mutate } from 'swr';
+import useSWR from "swr";
 import { fetcher } from "@/core/http";
 import { Button } from "@nextui-org/react";
 import { store } from "@/redux/store";
 import { changeModelStatus } from "@/redux/model/controller";
-import Timer from "../core/Timer";
-import { showErrorThunk } from "@/redux/toast/controller";
 
 const HeroSection = () => {
-  const { data, isLoading, } = useSWR("/me/leaderboard", fetcher);
+  const { data, isLoading } = useSWR("/me/leaderboard", fetcher);
 
   return (
     <section className="w-full relative">
@@ -21,14 +19,16 @@ const HeroSection = () => {
         <AppContainer className="flex flex-col h-full justify-center gap-14 py-20 min-h-[calc(100vh-154px)]">
           <div className="flex items-center justify-center flex-col gap-2 max-sm:items-start">
             <h1 className="text-5xl leading-[125%] text-center font-semibold capitalize max-sm:text-start">
-              Most wins gets R100 <br className="max-sm:hidden" /> in {data?.data ? <Timer data={data?.data} onComplete={() => {
-                mutate("/me/leaderboard");
-              }} /> : "00:00"}
+              Payment for User with most <br /> wins will be via email
             </h1>
-            <p className="text-base text-center max-sm:text-start">Payment for User with most wins will be via email</p>
-            <Button color="primary" className="max-w-48 mt-4" onClick={() => {
-              store.dispatch(changeModelStatus('leader-board'))
-            }} fullWidth>
+            <Button
+              color="primary"
+              className="max-w-48 mt-4"
+              onClick={() => {
+                store.dispatch(changeModelStatus("leader-board"));
+              }}
+              fullWidth
+            >
               Leader Board
             </Button>
           </div>
